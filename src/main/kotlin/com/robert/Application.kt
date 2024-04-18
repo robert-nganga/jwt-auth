223c0b26585d930ac191d6dae8c8f3862c726b8d
robert-nganga <robertnganga220@gmail.com>
2024-04-18 15:05:46 +0300

    changed aplication configuration

--- src/main/kotlin/com/robert/Application.kt
+++ src/main/kotlin/com/robert/Application.kt
@@ -2,12 +2,15 @@ package com.robert
 
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import com.robert.domain.models.User
 import com.robert.infrastructure.repository.UserRepositoryImpl
 import com.robert.plugins.*
 import com.robert.security.hashing.SHA256HashingService
 import com.robert.security.tokens.JwtTokenService
 import com.robert.security.tokens.TokenConfig
 import io.ktor.server.application.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
@@ -15,14 +18,17 @@ fun main(args: Array<String>) {
 
 @Suppress("unused")
 fun Application.module() {
-    val db = createMongoDb("", "")
+    val db = createMongoDb(
+        "mongodb+srv://admin:WA9nganga9@cluster0.wqakbcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        "Cluster0"
+    )
     val userDataSource = UserRepositoryImpl(db)
     val tokenService = JwtTokenService()
     val tokenConfig = TokenConfig(
-        issuer = environment.config.property("jwt.issuer").getString(),
-        audience = environment.config.property("jwt.audience").getString(),
+        issuer = "",//environment.config.property("jwt.issuer").getString(),
+        audience = "",//environment.config.property("jwt.audience").getString(),
         expiresIn = 365L * 1000L * 60L * 60L * 24L,
-        secret = System.getenv("JWT_SECRET")
+        secret = ""//System.getenv("JWT_SECRET")
     )
     val hashingService = SHA256HashingService()
     configureMonitoring()
